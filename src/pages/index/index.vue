f8e14112ff767c302ad84685bfa64b6b9cc2f71e
PC-20181204WGTS\Administrator <wx3364@163.com>
2019-04-19 17:15:43 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main-container">
+    <div class="header">
+      这是首页
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.main-container {
+  .header {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/pages/logs/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    这是订单页
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+  
+    }
+  },
+
+  created () {
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
